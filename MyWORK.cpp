8323ead28990a7be96374dd0fd99933334ce44da
vadimholtobin <74014049+vadimholtobin@users.noreply.github.com>
2020-11-05 21:11:11 +0300

    rabota
    rabota

---
+++ MyWORK.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+class Resources
+{
+public:
+    ~Resources() { cout << "destroy object's\n"; }
+};
+
+Resources& getRes()
+{
+    static Resources r1;
+    return r1;
+}
+
+void makeChild(const string& command)
+{
+    int pid = fork();
+    switch (pid)
+    {
+    case -1:
+        cout << "error!\n";
+        return;
+    case 0:
+        cout << "\n";
+        return;
+    }
+    cout << "Теперь мы Ребенок\n";
+    throw command;
+}
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        Resources& ref = getRes();
+        makeChild("ребенок");
+    }
+    catch (const string& command)
+    {
+        execl(command.c_str(), "");
+    }
+    return 0;
+}
+
+
